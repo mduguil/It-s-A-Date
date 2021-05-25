12358097b1075fcac0dc80554b6d7738f847e2da
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-25 11:56:51 -0700

    removed commented out code

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-// import Home from './pages/home';
 import DateForm from '../client/components/dateForm';
 
 export default class App extends React.Component {
   render() {
-    // return <Home />;
     return <DateForm />;
   }
 }
