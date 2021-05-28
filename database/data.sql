841cfe4d11316118b0eeec43ad0160b2aa2a0e14
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-28 13:27:34 -0700

    created user data

---
+++ database/data.sql
@@ -0,0 +1,2 @@
+insert into "user" ("name", "phoneNumber")
+values ('User1', '1234567890')
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,22 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "user" (
+  "userId"   serial,
+  "name"      text           not null,
+  "phoneNumber"    integer           not null,
+  "friendId"     serial        not null,
+  "createdAt" timestamptz(6) not null default now(),
+  primary key ("userId")
+);
+
+
+-- create table "contacts" (
+--   "userId"   serial,
+--   "name"      text           not null,
+--   "phoneNumber"    integer           not null,
+--   "friendId"     integer        not null,
+--   "createdAt" timestamptz(6) not null default now(),
+--   primary key ("userId")
+-- );
