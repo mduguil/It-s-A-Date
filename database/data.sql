ead6eb6b233d5d56af6a3714a549f7b870bee065
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-28 15:47:02 -0700

    created dates table

--- database/data.sql
+++ database/data.sql
@@ -1,6 +1,12 @@
-insert into "user" ("name", "phoneNumber")
+insert into "users" ("name", "phoneNumber")
 values ('User1', '1234567890');
 
 insert into "contacts" ("name", "phoneNumber")
 values ('Friend1', '1234456789'),
-       ('Friend2', '2234567890')
+       ('Friend2', '2234567890');
+
+insert into "dates" ("location", "day", "time", "activity", "notes")
+values ('5258 Riverside Dr, Chino, CA 91710, United States', '12 6 2021', '15:30', 'Hiking', 'Bring lots of water!');
+
+insert into "dates" ("location", "day", "time", "activity")
+values ('5258 Riverside Dr, Chino, CA 91710, United States', '12 6 2021', '15:30', 'Eating');
--- database/schema.sql
+++ database/schema.sql
@@ -6,20 +6,31 @@ drop schema "public" cascade;
 
 create schema "public";
 
-create table "user" (
-  "userId"   serial,
-  "name"      text           not null,
-  "phoneNumber"    numeric   not null,
-  "friendId"     serial      not null,
-  "createdAt" timestamptz(6) not null default now(),
+create table "users" (
+  "userId"         serial,
+  "name"           text           not null,
+  "phoneNumber"    numeric        not null,
+  "friendId"       serial         not null,
+  "createdAt"      timestamptz(6) not null default now(),
   primary key ("userId")
 );
 
 
 create table "contacts" (
-  "friendId"   serial,
-  "name"      text           not null,
-  "phoneNumber"    numeric   not null,
-  "createdAt" timestamptz(6) not null default now(),
+  "friendId"    serial,
+  "name"        text           not null,
+  "phoneNumber" numeric        not null,
+  "createdAt"   timestamptz(6) not null default now(),
   primary key ("friendId")
+);
+
+create table "dates" (
+  "dateId"     serial,
+  "location"   text           not null,
+  "day"        text           not null,
+  "time"       text           not null,
+  "activity"   text           not null,
+  "notes"      text,
+  "createdAt"  timestamptz(6) not null default now(),
+  primary key ("dateId")
 );
