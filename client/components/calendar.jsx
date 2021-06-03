7e553e6defb8c9a36bb489c5ff18b93e08f8d19b
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-03 16:59:07 -0700

    can view previous and next month

--- client/components/calendar.jsx
+++ client/components/calendar.jsx
@@ -2,32 +2,71 @@ import React from 'react';
 import Navbar from './navbar';
 import moment from 'moment';
 
+function generateCalendarDays(startDay, endDay) {
+  const calendarDays = [];
+
+  const day = startDay.clone().subtract(1, 'day');
+  while (day.isBefore(endDay, 'day')) {
+    calendarDays.push(
+      Array(7).fill('dayOfTheWeek').map(() => day.add(1, 'day').clone())
+    );
+  }
+
+  return calendarDays;
+}
+
+function getStartEndDay(currMonth) {
+  return {
+    startDay: currMonth.clone().startOf('month').startOf('week'),
+    endDay: currMonth.clone().endOf('month').endOf('week')
+  };
+}
+
 export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
+    const currMonth = moment();
+
     const daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const startDay = moment().clone().startOf('month').startOf('week');
-    const endDay = moment().clone().endOf('month').endOf('week');
-    const day = startDay.clone().subtract(1, 'day');
+    const { startDay, endDay } = getStartEndDay(currMonth);
 
     this.state = {
       daysOfTheWeek,
       startDay,
       endDay,
-      day,
-      calendar: []
+      calendar: [],
+      selectedDay: null,
+      currMonth: currMonth,
+      calendarDays: generateCalendarDays(startDay, endDay)
     };
-    this.getWeeklyDates = this.getWeeklyDates.bind(this);
-    this.isToday = this.isToday.bind(this);
+
+    this.prevMonth = this.prevMonth.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
 
   }
 
-  getWeeklyDates() {
-    while (this.state.day.isBefore(this.state.endDay, 'day')) {
-      this.state.calendar.push(
-        Array(7).fill('dayOfTheWeek').map(() => this.state.day.add(1, 'day').clone())
-      );
-    }
+  prevMonth() {
+    const currMonth = this.state.currMonth.clone().subtract(1, 'month');
+    const { startDay, endDay } = getStartEndDay(currMonth);
+
+    this.setState({
+      currMonth,
+      calendarDays: generateCalendarDays(startDay, endDay)
+    });
+  }
+
+  nextMonth() {
+    const currMonth = this.state.currMonth.clone().add(1, 'month');
+    const { startDay, endDay } = getStartEndDay(currMonth);
+
+    this.setState({
+      currMonth,
+      calendarDays: generateCalendarDays(startDay, endDay)
+    });
+  }
+
+  isSelected(day) {
+    return moment(day).isSame(day, 'day');
   }
 
   isToday(day) {
@@ -36,17 +75,19 @@ export default class Calendar extends React.Component {
 
   dayStyle(day) {
     if (this.isToday(day)) return 'today';
+    if (this.isSelected(day)) return 'selected-day';
     return '';
   }
 
   render() {
-    this.getWeeklyDates();
     return (
       <div className="container">
         <div className="calendar -container">
           <h1 className="calendar-title center row">Calendar</h1>
           <div className="month-name-container">
-            <div className="month-name">June</div>
+            <i className="fas fa-chevron-left" onClick={() => this.prevMonth()}></i>
+            <div className="month-name">{this.state.currMonth.format('MMMM')}</div>
+            <i className="fas fa-chevron-right" onClick={() => this.nextMonth()}></i>
           </div>
           <div className="days-of-the-week">
             {this.state.daysOfTheWeek.map(day => {
@@ -56,13 +97,17 @@ export default class Calendar extends React.Component {
             })}
           </div>
           <div className="calendar">
-            {this.state.calendar.map((week, wi) => {
+            {this.state.calendarDays.map((week, wi) => {
               return (
                 <div key={wi} className="week">
                   {week.map((day, di) => {
                     return (
-                      <div key={di} className="day-number">
-                        <div className={this.dayStyle(day)}>
+                      <div
+                        key={di}
+                        className="day-number"
+                        onClick={event => { this.isSelected(day); }}
+                      >
+                        <div value={day.format('D')} className={this.dayStyle(day)}>
                           {day.format('D')}
                         </div>
                       </div>
@@ -73,7 +118,7 @@ export default class Calendar extends React.Component {
             })}
           </div>
         </div>
-        <Navbar calendarIcon="far fa-calendar nav-icon home-calendar-icon"/>
+        <Navbar calendarIcon="far fa-calendar nav-icon home-calendar-icon" />
       </div>
     );
   }
