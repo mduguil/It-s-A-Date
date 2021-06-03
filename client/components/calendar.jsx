9185d8d7fa8d268a351dac038f32766f647cc7ae
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-03 13:08:23 -0700

    renders days of the month

--- client/components/calendar.jsx
+++ client/components/calendar.jsx
@@ -1,19 +1,64 @@
 import React from 'react';
 import Navbar from './navbar';
+import moment from 'moment';
 
 export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    const daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const startDay = moment().clone().startOf('month').startOf('week');
+    const endDay = moment().clone().endOf('month').endOf('week');
+    const day = startDay.clone().subtract(1, 'day');
 
+    this.state = {
+      daysOfTheWeek,
+      startDay,
+      endDay,
+      day,
+      calendar: []
     };
+    this.getWeeklyDates = this.getWeeklyDates.bind(this);
+  }
+
+  getWeeklyDates() {
+    while (this.state.day.isBefore(this.state.endDay, 'day')) {
+      this.state.calendar.push(
+        Array(7).fill('dayOfTheWeek').map(() => this.state.day.add(1, 'day').clone())
+      );
+    }
   }
 
   render() {
+    this.getWeeklyDates();
     return (
       <div className="container">
         <div className="calendar -container">
           <h1 className="calendar-title center row">Calendar</h1>
+          <div className="month-name-container">
+            <div className="month-name">June</div>
+          </div>
+          <div className="days-of-the-week">
+            {this.state.daysOfTheWeek.map(day => {
+              return (
+                <div className="weekday" key={day}>{day}</div>
+              );
+            })}
+          </div>
+              <div>
+                {this.state.calendar.map((week, wi) => {
+                  return (
+                    <div key={wi}>
+                      {week.map((day, di) => {
+                        return (
+                          <div key={di}>
+                            {day.format('D')}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
         </div>
         <Navbar calendarIcon="far fa-calendar nav-icon home-calendar-icon"/>
       </div>
