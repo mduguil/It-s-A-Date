da2d63f5d7b3f5fa35be52bba537000aaa1980d0
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-10 16:17:33 -0700

    notes renders

--- client/components/calendar/showDatesScheduled.jsx
+++ client/components/calendar/showDatesScheduled.jsx
@@ -4,27 +4,48 @@ import { hasDateScheduled } from './utils';
 export default class ShowDatesScheduled extends React.Component {
   render() {
     return (
-      <div className="scheduled-date-container">
+      <div className="day-scheduled-date-container">
         {hasDateScheduled(this.props.selectedDay, this.props.byDate)
           ? <>
               <div className="scheduled-date-title">
                 Dates Today
               </div>
-              <div className="schedule">
+              <div className="schedule-container">
                 {this.props.byDate[this.props.selectedDay.format('M D YYYY')]
                   .map((date, i) => {
                     return (
-                      <>
-                        <div className="scheduled-invitees" key={i}>
-                          {date.invites.toString()}
+                      <div className="schedule" key={i}>
+                        <div className="scheduled-activity-container">
+                          <div className="scheduled-activity"></div>
+                          <div className="activity-decor"></div>
                         </div>
-                        <div className="scheduled-time" key={i}>
-                          {date.time}
+                          <div className="scheduled-date-info-container">
+                            <div className="scheduled-invitees-container">
+                              <div className="scheduled-invitees-img-container">
+                                <img className="scheduled-invitees-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"></img>
+                              </div>
+                              <div className="scheduled-invitees-time-container">
+                                <div className="scheduled-invitees">
+                                  <div className="scheduled-invitees" key={i}>
+                                    {date.invites}
+                                  </div>
+                                </div>
+                                <div className="scheduled-time" key={i}>
+                                  {date.time}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="scheduled-location" key={i}>
+                              {date.location}
+                          </div>
+                          {date.notes
+                            ? <div className="scheduled-note" key={i}>
+                              <span className="note-label">NOTE:</span> {date.notes}
+                              </div>
+                            : ''
+                          }
                         </div>
-                        <div className="scheduled-location" key={i}>
-                          {date.location}
-                        </div>
-                      </>
+                      </div>
                     );
                   })
                 }
