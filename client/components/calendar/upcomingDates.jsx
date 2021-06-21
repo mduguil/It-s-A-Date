e69b4bc9de24d6af1daf6740a009b184d69f0940
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-21 15:46:50 -0700

    upcoming date info style

--- client/components/calendar/upcomingDates.jsx
+++ client/components/calendar/upcomingDates.jsx
@@ -15,24 +15,20 @@ export default class UpcomingDates extends React.Component {
               {getFutureDates(this.props.byDate).map((date, i) => {
                 return (
                   <div className="upcoming-date-schedule" key={i}>
-                    <div className="scheduled-activity-container">
+                    <div className="upcoming-date-activity-container">
                       <div className={styleDailyScheduleActivity(date.activity)}>
                         <i className={activityIcons[date.activity.split(' ')[0].toLowerCase()]}></i>
                       </div>
                     </div>
-                    <div className="scheduled-date-info-container">
-                      <div className="scheduled-invitees-container">
-                        <div className="upcoming-date-info-container">
-                          <div className="scheduled-invitees" key="invite">
-                            {date.activity} <span className="filler-word">on</span> {date.day}
-                          </div>
-                          <div className="scheduled-time" key="time">
-                            {date.time}
-                          </div>
-                          <div className="scheduled-invites-container" key="time">
-                            With <span className="scheduled-invitees">{date.invites}</span>
-                          </div>
-                        </div>
+                    <div className="upcoming-date-info-container">
+                      <div className="upcoming-dates-invitees" key="invite">
+                        {date.activity} <span className="filler-word">on</span> {date.day}
+                      </div>
+                      <div className="scheduled-time" key="time">
+                        {date.time}
+                      </div>
+                      <div className="scheduled-invites-container" key="time">
+                        With <span className="scheduled-invitees">{date.invites}</span>
                       </div>
                     </div>
                   </div>
