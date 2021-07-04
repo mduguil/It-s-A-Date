343acc2d888255649623ace1e99c46761f5bdfc0
Mikaela Duguil <kimmieduggz@gmail.com>
2021-07-04 00:05:08 -0700

    checks for err or fetch

--- client/components/contactsListModal.jsx
+++ client/components/contactsListModal.jsx
@@ -19,7 +19,7 @@ export default class ContactsListModal extends React.Component {
 
   componentDidMount() {
     this.setState({
-      isfetching: true
+      isFetching: true
     });
     fetch('/api/contacts')
       .then(res => res.json())
@@ -76,74 +76,70 @@ export default class ContactsListModal extends React.Component {
 
   render() {
     return (
-      <>
-      {this.state.err
-        ? <div>{this.state.err}</div>
-        : <>
-            {this.state.addContactFormIsOpen
-              ? <AddContactsForm
-                name={this.state.name}
-                number={this.state.number}
-                handleSubmit={event => this.handleSubmit(event)}
-                handleNameChange={event => {
-                  this.setState({
-                    name: event.target.value
-                  });
-                }}
-                handleNumChange={event => {
-                  this.setState({
-                    number: event.target.value
-                  });
-                }}
-                handleCancel={event => {
-                  this.closeAddForm();
-                }}
-              />
-              : <div className="contacts-container container">
-                <div className="contacts-title-container">
-                  <i className="fas fa-arrow-left center back-icon"/>
-                  <h1 className="contacts-title center row">Contacts</h1>
-                  <i className="fas fa-plus center add-icon"
-                    onClick={event => {
-                      this.setState({
-                        addContactFormIsOpen: true
-                      });
-                    }}
-                  />
-                </div>
-                <div>
-                  {this.state.isFetching
-                    ? <div className="loading-placeholder center">Loading...</div>
-                    : <>
-                      {this.state.contacts.map(
-                        contact => {
-                          return (
-                            <div
-                              className="contact-container"
-                              onClick={() => { this.props.handleClick(contact); }}
-                              key={contact.contactId}
-                            >
-                              <div className="contact-img-container">
-                                <img className="contact-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" />
-                              </div>
-                              <div className="contact-info">
-                                <div className="contact-name">{contact.name}</div>
-                                <div className="contact-number">{`(${contact.phoneNumber.slice(0, 3)}) ${contact.phoneNumber.slice(3, 6)} - ${contact.phoneNumber.slice(6, 10)}`}
-                                </div>
-                              </div>
+    <>
+      {this.state.addContactFormIsOpen
+        ? <AddContactsForm
+          name={this.state.name}
+          number={this.state.number}
+          handleSubmit={event => this.handleSubmit(event)}
+          handleNameChange={event => {
+            this.setState({
+              name: event.target.value
+            });
+          }}
+          handleNumChange={event => {
+            this.setState({
+              number: event.target.value
+            });
+          }}
+          handleCancel={event => {
+            this.closeAddForm();
+          }}
+        />
+        : <div className="contacts-container container">
+          <div className="contacts-title-container">
+            <i className="fas fa-arrow-left center back-icon"/>
+            <h1 className="contacts-title center row">Contacts</h1>
+            <i className="fas fa-plus center add-icon"
+              onClick={event => {
+                this.setState({
+                  addContactFormIsOpen: true
+                });
+              }}
+            />
+          </div>
+          <div>
+            {this.state.err
+              ? <div>{this.state.err}</div>
+              : <>
+                  {this.state.isFetching && <div className="loading-placeholder center">Loading...</div>}
+                  {this.state.contacts.map(
+                    contact => {
+                      return (
+                        <div
+                          className="contact-container"
+                          onClick={() => { this.props.handleClick(contact); }}
+                          key={contact.contactId}
+                        >
+                          <div className="contact-img-container">
+                            <img className="contact-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" />
+                          </div>
+                          <div className="contact-info">
+                            <div className="contact-name">{contact.name}</div>
+                            <div className="contact-number">{`(${contact.phoneNumber.slice(0, 3)}) ${contact.phoneNumber.slice(3, 6)} - ${contact.phoneNumber.slice(6, 10)}`}
                             </div>
-                          );
-                        }
-                      )
-                      }
-                      </>
+                          </div>
+                        </div>
+                      );
+                    }
+                  )
                   }
-                </div>
-              </div>
+                </>
             }
-          </>
-        }
-      </>
+          </div>
+        </div>
+      }
+    </>
     );
   }
 }
