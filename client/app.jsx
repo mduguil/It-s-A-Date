a4e4b02a0813ff71f8f6a45efdbb83ea6a4c709a
Mikaela Duguil <kimmieduggz@gmail.com>
2021-07-07 19:29:50 -0700

    logo on date form

--- client/app.jsx
+++ client/app.jsx
@@ -23,42 +23,42 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-      <div className="header-container">
-        <div className="logo-container">
-          <img className="logo" src="/logo.png" />
+        <div className="header-container">
+          <div className="logo-container">
+            <img className="logo" src="/logo.png" />
+          </div>
+          <div className="slogan-container">
+            <div className="slogan">Schedule dates with friends</div>
+          </div>
         </div>
-        <div className="slogan-container">
-          <div className="slogan">Schedule dates with friends</div>
-        </div>
-      </div>
-      <Router>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/weekly-view/:day">
-              <Home weeklyView={true}/>
-            </Route>
-            <Route path="/weekly-view">
-              <Home weeklyView={true} />
-            </Route>
-            <Route path="/date-form/:dateId">
-              <DateFormContainer />
-            </Route>
-            <Route path="/date-form">
-              <Date />
-            </Route>
-            <Route path="/invitations">
-              <Invitations />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/weekly-view/:day">
+                <Home weeklyView={true}/>
+              </Route>
+              <Route path="/weekly-view">
+                <Home weeklyView={true} />
+              </Route>
+              <Route path="/date-form/:dateId">
+                <DateFormContainer />
+              </Route>
+              <Route path="/date-form">
+                <Date />
+              </Route>
+              <Route path="/invitations">
+                <Invitations />
+              </Route>
+              <Route path="/settings">
+                <Settings />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
