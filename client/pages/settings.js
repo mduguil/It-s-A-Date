3d7ede4066cf700c4fe653883a4321cd4e5abe58
Mikaela Duguil <kimmieduggz@gmail.com>
2021-07-07 19:31:22 -0700

    logo on settings

--- client/pages/settings.js
+++ client/pages/settings.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default class Settings extends React.Component {
   render() {
     return (
-      <div className="container">
+      <>
         <div className="wip row center"> Work In Progress</div>
-      </div>
+      </>
     );
   }
 }
